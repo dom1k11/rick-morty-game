c27bfb103c6302ee811c6b7a6ef495148887f58a
Dominik Sedusov <dom1k11@outlook.com>
2025-09-23 14:31:51 +0300

    morty name args added

--- core/ParseArgs.js
+++ core/ParseArgs.js
@@ -1,10 +1,11 @@
 export class ArgsParser {
-  //TODO: Add morty as arg
+
   constructor(argv) {
     if (argv.length < 3) {
       throw new Error("Usage: node index.js <boxes>");
     }
     this.n = parseInt(argv[2], 10);
+    this.mortyClassName = argv[3]
     if (isNaN(this.n) || this.n < 3) {
       throw new Error("Number of boxes must be >= 3");
     }
