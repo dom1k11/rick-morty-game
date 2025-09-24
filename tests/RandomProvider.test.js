b502f306368e1a202211637545d01790f72b16bf
Dominik Sedusov <dom1k11@outlook.com>
2025-09-24 10:13:28 +0300

    test for hmac

---
+++ tests/RandomProvider.test.js
@@ -0,0 +1,38 @@
+import { describe, it, expect } from "vitest";
+import { RandomProvider } from "./core/RandomProvider.js";
+
+describe("RandomProvider", () => {
+  it("should generate valid HMAC and verify it", () => {
+    const random = new RandomProvider(10);
+
+    const hmac = random.generateHide();
+    expect(typeof hmac).toBe("string");
+    expect(hmac.length).toBeGreaterThan(0);
+
+    const { mortyValue, key } = random.revealKey();
+    expect(typeof mortyValue).toBe("number");
+    expect(typeof key).toBe("string");
+
+    const { recalculated, honest } = random.verify(hmac, mortyValue, key);
+    expect(recalculated).toBe(hmac);
+    expect(honest).toBe(true);
+  });
+  it("should fail verification is the key wrong", () => {
+    const random = new RandomProvider(5);
+    const hmac = random.generateHide();
+    const { mortyValue } = random.revealKey();
+    const wrongKey = "00".repeat(32);
+    const { honest } = random.verify(hmac, mortyValue, wrongKey);
+    expect(honest).toBe(false);
+  });
+  it("should fail verification with wrong mortyValue", () => {
+    const random = new RandomProvider(5);
+    const hmac = random.generateHide();
+    const { key } = random.revealKey();
+
+    const wrongValue = random.mortyValue + 1;
+    const { honest } = random.verify(hmac, wrongValue, key);
+
+    expect(honest).toBe(false);
+  });
+});
