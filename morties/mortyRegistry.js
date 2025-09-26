926a5d89828027c3502aa3898c7e855d9160f05c
Dominik Sedusov <dom1k11@outlook.com>
2025-09-26 09:19:40 +0300

    reading morties from folder

--- morties/mortyRegistry.js
+++ morties/mortyRegistry.js
@@ -1,9 +1,26 @@
-import { ClassicMorty } from "./ClassicMorty.js";
-import { LazyMorty } from "./LazyMorty.js";
-import { EvilMorty } from "./EvilMorty.js";
+import fs from "fs";
+import path from "path";
+import { pathToFileURL } from "url";
 
-export const mortyRegistry = {
-  ClassicMorty,
-  LazyMorty,
-  EvilMorty,
-};
+const mortyDir = path.resolve("./morties");
+
+export const mortyRegistry = {};
+
+async function loadMorties() {
+  for (const file of fs.readdirSync(mortyDir)) {
+    if (file.endsWith("Morty.js")) {
+      const absPath = path.join(mortyDir, file);
+      const modulePath = pathToFileURL(absPath).href;
+
+      const module = await import(modulePath);
+
+      const className = path.basename(file, ".js");
+      const MortyClass = module[className];
+      if (MortyClass) {
+        mortyRegistry[className] = MortyClass;
+      }
+    }
+  }
+}
+
+await loadMorties();
